f4bb47bb532f92c8518bfd12365b50f0c94049d7
monisharema <monisharema97@gmail.com>
2026-03-29 20:42:21 +0300

    setup: prisma client setup

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -1,8 +1,12 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from "../../app/config/env";
+import {PrismaClient} from "../../../generated/prisma/client.js"
 
 
 const adapter = new PrismaPg({
     connectionString: config.DATABASE_URL!
 })
 
+export const prisma = new PrismaClient({
+    adapter 
+})
